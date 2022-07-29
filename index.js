3dc19015de700c2e04bf0188f553ee165c5d8670
Alan <holasoyalan9@gmail.com>
2022-07-28 20:50:00 -0500

    Masomenos hace lo que queremos jsjs

--- index.js
+++ index.js
@@ -4,27 +4,25 @@ const getData = async (url) => {
   // console.log(data);
   return data;
 }
-// const bar_mon = document.getElementById('bar_mon');
-const amount_mon = document.getElementById('amount_mon');
-const amount_tue = document.getElementById('amount_tue');
-const amount_wed = document.getElementById('amount_wed');
-const amount_thu = document.getElementById('amount_thu');
-const amount_fri = document.getElementById('amount_fri');
-const amount_sat = document.getElementById('amount_sat');
-const amount_sun = document.getElementById('amount_sun');
 (async () => {
   try {
     const dates = await getData('./data.json');
-    amount_mon.innerText = `$${dates[0].amount}`;
-    amount_tue.innerText = `$${dates[1].amount}`;
-    amount_wed.innerText = `$${dates[2].amount}`;
-    amount_thu.innerText = `$${dates[3].amount}`;
-    amount_fri.innerText = `$${dates[4].amount}`;
-    amount_sat.innerText = `$${dates[5].amount}`;
-    amount_sun.innerText = `$${dates[6].amount}`;
-    // bar_mon.addEventListener('onmouseover', (event) =>{
-    //   event.t
-    // })
+    const bar = document.getElementById('bar_tue');
+
+    bar.addEventListener("mouseover", event => {
+      const container = event.target;
+      const amount = document.createElement('div');
+      const text = document.createTextNode(dates[0].amount);
+      amount.classList.add('amount');
+      amount.setAttribute('id', 'amount_mon');
+      amount.appendChild(text);
+      container.appendChild(amount);
+      console.log(container);
+    })
+    bar.addEventListener('mouseout', event =>{
+      event.target.innerText = ''
+      console.log(event.target);
+    })
     console.log(dates[0].amount)
   } catch (error) {
     console.error(error);
