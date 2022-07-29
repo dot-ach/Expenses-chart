6468d6e882a14f605d5e5024ef4f234914e473de
Alan <holasoyalan9@gmail.com>
2022-07-28 23:19:28 -0500

    Ya pudimos hacer que el cuadrito aparezca pero no que se quite, ahí vamos

--- index.js
+++ index.js
@@ -7,20 +7,35 @@ const getData = async (url) => {
 (async () => {
   try {
     const dates = await getData('./data.json');
-    const bar = document.getElementById('bar_tue');
+    const bar = document.getElementById('bar_mon');
+    // const 
 
     bar.addEventListener("mouseover", event => {
-      const container = event.target;
+      const container = document.getElementById('bar-container')
+      container.innerHTML = ''
+      console.log(container);
       const amount = document.createElement('div');
       const text = document.createTextNode(dates[0].amount);
       amount.classList.add('amount');
       amount.setAttribute('id', 'amount_mon');
       amount.appendChild(text);
       container.appendChild(amount);
-      console.log(container);
+
+      const bar = document.createElement('div');
+      bar.classList.add('graph-bar_mon')
+      bar.setAttribute('id', 'bar_mon')
+      container.appendChild(bar);
+
+      const paragraph = document.createElement('p')
+      const textDay = document.createTextNode('mon')
+      paragraph.appendChild(textDay);
+
+      container.appendChild(paragraph);
     })
     bar.addEventListener('mouseout', event =>{
-      event.target.innerText = ''
+      event.target.innerHTML = `<div class="graph-bar_container" id="bar-container">
+      <div class="graph-bar_mon" id="bar_mon"></div>
+      <p>mon</p>`
       console.log(event.target);
     })
     console.log(dates[0].amount)
